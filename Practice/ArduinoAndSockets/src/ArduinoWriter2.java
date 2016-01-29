4f7ea8b6e30ac554b5cadfc2ee9be7a64888ecc5
mkalinich <mkalinich@selectica.com>
2016-01-29 19:46:50 +0200

    Updated 'ArduinoWriter2' - sending the rotation angle of Servo from console to serial port

--- Practice/ArduinoAndSockets/src/ArduinoWriter2.java
+++ Practice/ArduinoAndSockets/src/ArduinoWriter2.java
@@ -12,7 +12,9 @@ public class ArduinoWriter2 {
 		System.exit(0);
 	}
 	SerialPort highestPort = new SerialPort(serialPortList[serialPortList.length-1]);
-	byte inputValue=0, prevValue=0;
+	int inputValue=0, prevValue=0;
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	String readString;
 	System.out.println("Available serial ports are:");
 	for (int i=0; i<serialPortList.length; i++){
 		System.out.println(serialPortList[i]);
@@ -26,12 +28,21 @@ public class ArduinoWriter2 {
 	highestPort.openPort();
 	highestPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 	while (true){
-		System.out.println("Enter the Servo desired angle:");
-		inputValue = (byte) System.in.read();
-		highestPort.writeByte(inputValue);
+		System.out.print("\nEnter the Servo desired angle: ");
+		//inputValue = (byte) System.in.read();
+		readString=reader.readLine();
+		if (readString=="q"||readString=="Q") break;
+		else{
+			inputValue = Integer.parseInt(readString);
+			highestPort.writeInt(inputValue);
+		}
+	}
+
+		//highestPort.writeByte(inputValue);
+		//highestPort.writeInt(150);
 
 	}
-	}
+
 	catch (SerialPortException spe) {
 		System.out.println("Error in Serial Port: " + spe);
 	}
@@ -41,6 +52,7 @@ public class ArduinoWriter2 {
 	finally{
       try{
     		highestPort.closePort();
+    		reader.close();
         }catch(Exception e){
        	 System.out.println("Error occured while closing the Serial Port: " + e.getMessage());
         }
